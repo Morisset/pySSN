029789ec9c4c988532f8217b7c29c85c8fc4f172
morisset <chris.morisset@gmail.com>
2016-06-22 00:52:18 +0200

    make_phyat

--- pyssn/phyat_lists/make_phyat_list.py
+++ pyssn/phyat_lists/make_phyat_list.py
@@ -6,6 +6,7 @@ Created on 21 juin 2016
 import numpy as np
 import pyneb as pn
 from pyneb.utils.physics import Z
+from pyneb.utils.misc import int_to_roman
 
 def print_liste_phyat(atom, tem, den, cut=1e-3, ij_ref = None):
     emis = atom.getEmissivity(tem, den)
@@ -15,12 +16,36 @@ def print_liste_phyat(atom, tem, den, cut=1e-3, ij_ref = None):
         emis_ref = emis[ij_ref[0]-1, ij_ref[1]-1]
     wls = atom.wave_Ang
     NLevels = emis.shape[0]
+    str_print = ' {:02d}{:02d}00000{:02d}{:02d} {:2s}_{:5s} {:11.3f} 0.000{:10.3e}  1.000  {:02d}{:02d}00000{:02d}{:02d}   1   1.00 {} {}'
     for i in np.arange(NLevels):
         emis_i = emis[i,:]
         j_emis_ref_loc = np.argmax(emis_i)
-        print('{}{:02d}000000{:02d}{:02d} {:10.2f} {:8.4f}'.format(Z[atom.elem], atom.spec, i+1, j_emis_ref_loc+1,wls[i,j_emis_ref_loc], emis[i, j_emis_ref_loc]/emis_ref)) 
+        print(str_print.format(Z[atom.elem], 
+                               atom.spec, 
+                               i+1, 
+                               0, 
+                               atom.elem,
+                               int_to_roman(atom.spec), 
+                               1.0, 
+                               emis[i, j_emis_ref_loc]/emis_ref,
+                               Z[atom.elem], 
+                               atom.spec, 
+                               0, 0,
+                               i+1, 
+                               j_emis_ref_loc+1))
         for j in np.arange(i):
             if emis[i,j] > cut * emis_ref:
-                
-                print('{}{:02d}000000{:02d}{:02d} {:10.2f} {:8.4f}'.format(Z[atom.elem], atom.spec, i+1, j+1, wls[i,j], emis[i,j]/emis[i, j_emis_ref_loc])) 
-    
+                print(str_print.format(Z[atom.elem], 
+                                       atom.spec, 
+                                       i+1, 
+                                       j+1, 
+                                       atom.elem, 
+                                       int_to_roman(atom.spec), 
+                                       wls[i,j], 
+                                       emis[i,j]/emis[i, j_emis_ref_loc], Z[atom.elem], 
+                                       atom.spec, 
+                                       i+1, 0, 
+                                       i+1, 
+                                       j+1))
+
+
