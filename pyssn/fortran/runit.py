f16d9b31f305c4f064a0c50c8bb5fd25e00fe686
morisset <chris.morisset@gmail.com>
2018-04-23 18:37:12 +0200

    small change

--- pyssn/fortran/runit.py
+++ pyssn/fortran/runit.py
@@ -3,13 +3,13 @@ Created on 1 oct. 2017
 
 @author: christophemorisset
 '''
-from shutil import copyfile
-from ..phyat_lists.entries import execution_path
+import shutil
 import subprocess
 import sys, os
+from ..phyat_lists.entries import execution_path
 
 def run_XSSN(outputcond_file):
-    copyfile(outputcond_file, execution_path('outputcond.dat'))
+    shutil.copyfile(outputcond_file, execution_path('outputcond.dat'))
     to_run = 'cd {} ; ./XSSN_Phyat.exe'.format(os.path.join(os.path.dirname(sys._getframe(1).f_code.co_filename), '../fortran/'))
     stdout = open('XSSN.out', 'w')   
     process = subprocess.Popen(to_run, shell=True, stdout=stdout, close_fds=True)
