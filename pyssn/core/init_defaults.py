e78217e041a49b74b1ce43a4d71bb9cfeed7cd8e
mvfcopetti <mvfcopetti@gmail.com>
2019-02-04 22:28:45 -0200

    Instrumental profile dialog created

--- pyssn/core/init_defaults.py
+++ pyssn/core/init_defaults.py
@@ -117,15 +117,10 @@ limit_sp = [0, 1.0e+10]
 wave_unit = 'Angstrom'
 
 # Here are the 2 filenames of the data. The filename 
-# of the atomic database is hard-coded 
 fic_modele = 'liste_modele.dat'
+fic_cosmetik = 'liste_cosmetik.dat'
 fic_cosmetik = None
-
-# File containing the definition of the intrinsc line profile
-fic_profile = None
-
-# File containing the definition of the instrumental profile
-fic_instr_prof = None
+phyat_file = None
 
 # To take into account 'fic_cosmetik' data
 do_cosmetik = True
--- pyssn/core/profiles.py
+++ pyssn/core/profiles.py
@@ -37,7 +37,6 @@ def profil_emis(w, raie, lambda_shift=0.):
     else:
         profil = gauss(1., 0., 1.)  
         T4 = 1.
-
     if T4 > 0.0:
         fwhm_therm = 21.4721 * np.sqrt(T4 / masse) * lambda_0 / 299792.50 #km/s
         profil = convolgauss(profil, w, lambda_0, fwhm_therm)
@@ -46,20 +45,31 @@ def profil_emis(w, raie, lambda_shift=0.):
 
     
     return profil
+
+def translate_intr_prof(prof):    
     
+    keys = prof.keys()
+    old_keys = [ 'largeur', 'B_1l', 'B_2l', 'B_3l', 'B_4l', 'B_1r', 'B_2r', 'B_3r', 'B_4r', 'decroiss_1', 'decroiss_2', 'decroiss_3', 'decroiss_4']
+    new_keys = [ 'width', 'Bb_1', 'Bb_2', 'Bb_3', 'Bb_4', 'Br_1', 'Br_2', 'Br_3', 'Br_4', 'beta_1', 'beta_2', 'beta_3', 'beta_4' ]
+    key_dict = dict(zip(old_keys, new_keys))
+    for key in old_keys:
+        if key in keys:
+            prof[key_dict[key]] = prof[key]
+    return prof
 
 def profil_instr(filter_size, prof, lambda_pix):
     
+    prof = translate_intr_prof(prof)
     w_norm = np.arange(filter_size)-filter_size/2
     w_norm_abs = np.abs(w_norm)
     profil = np.zeros(filter_size)
     prof_add = lambda dec, alpha, Bl, Br: (Bl*winf + Br*wsup)*np.exp(-(w_norm_abs/dec*lambda_pix)**alpha)
-    
-    if prof['largeur'] > 0:
-        profil = np.exp(-(w_norm/prof['largeur']*lambda_pix)**2)
+
+    if prof['width'] > 0:
+        profil = np.exp(-(w_norm/prof['width']*lambda_pix)**2)
     else:
         profil[filter_size/2] = 1.0
-        profil[w_norm_abs <= abs(prof['largeur']/lambda_pix)] = 1.0
+        profil[w_norm_abs <= abs(prof['width']/lambda_pix)] = 1.0
     
     winf = np.zeros(filter_size)
     wsup = np.zeros(filter_size)
@@ -67,9 +77,7 @@ def profil_instr(filter_size, prof, lambda_pix):
     winf[w_norm <= 0.0] = 1.0
     wsup[w_norm > 0.0] = 1.0
 
-    profil += prof_add(prof['decroiss_1'], prof['alpha_1'], prof['B_1l'], prof['B_1r'])
-    profil += prof_add(prof['decroiss_2'], prof['alpha_2'], prof['B_2l'], prof['B_2r'])
-    profil += prof_add(prof['decroiss_3'], prof['alpha_3'], prof['B_3l'], prof['B_3r'])
-    profil += prof_add(prof['decroiss_4'], prof['alpha_4'], prof['B_4l'], prof['B_4r'])
-    
+    indexes = sorted([indexed_key.replace('alpha','') for indexed_key in prof.keys() if 'alpha' in indexed_key])
+    for i in indexes:
+        profil += prof_add(prof['beta'+i], prof['alpha'+i], prof['Bb'+i], prof['Br'+i])
     return profil
